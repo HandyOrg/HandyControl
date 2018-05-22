09766e18ebd8820f1fd6447ebb5015315e6874af
纳边 <836904362@qq.com>
2018-05-22 13:26:36 +0800

    添加demo项目
    添加demo项目
    
    Co-Authored-By: NaBian <836904362@qq.com>

---
+++ HandyControlDemo/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HandyControlDemo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ HandyControlDemo/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace HandyControlDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
