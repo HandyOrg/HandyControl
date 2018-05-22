66e2c319a3eab943b14146bcb8bf2e81500bb983
纳边 <836904362@qq.com>
2018-05-22 13:18:08 +0800

    添加项目
    添加项目

---
+++ HandyControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HandyControl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HandyControl")]
+[assembly: AssemblyCopyright("Copyright © 纳边 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
