02201b4f658c1c9411f8c347f25223aa85bb536a
NaBian <cj2@zj-r.com>
2018-08-23 19:49:08 +0800

    add Expander

---
+++ HandyControl/Controls/Attach/StatusSwitchElement.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+// ReSharper disable once CheckNamespace
+namespace HandyControl.Controls
+{
+    public class StatusSwitchElement
+    {
+        public static readonly DependencyProperty CheckedElementProperty = DependencyProperty.RegisterAttached(
+            "CheckedElement", typeof(object), typeof(StatusSwitchElement), new PropertyMetadata(default(object)));
+
+        public static void SetCheckedElement(DependencyObject element, object value) => element.SetValue(CheckedElementProperty, value);
+
+        public static object GetCheckedElement(DependencyObject element) => element.GetValue(CheckedElementProperty);
+    }
+}
