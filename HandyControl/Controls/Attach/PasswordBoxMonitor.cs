67036420640aa1f53eafdbf1c0091f201a72c260
Mahdi Hosseini <mahdidvb72@gmail.com>
2018-12-09 22:37:33 +0330

    add ShowPassword Property for PassordBox
    now we can show or hide password with toggle button

--- HandyControl/Controls/Attach/PasswordBoxMonitor.cs
+++ HandyControl/Controls/Attach/PasswordBoxMonitor.cs
@@ -50,5 +50,89 @@ namespace HandyControl.Controls
                 SetPasswordLength(passwordBox, passwordBox.Password.Length);
             }
         }
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached("Password",
+            typeof(string), typeof(PasswordBoxMonitor),
+            new FrameworkPropertyMetadata(string.Empty, OnPasswordPropertyChanged));
+
+        public static readonly DependencyProperty AttachProperty =
+            DependencyProperty.RegisterAttached("Attach",
+            typeof(bool), typeof(PasswordBoxMonitor), new PropertyMetadata(false, Attach));
+
+        private static readonly DependencyProperty IsUpdatingProperty =
+           DependencyProperty.RegisterAttached("IsUpdating", typeof(bool),
+           typeof(PasswordBoxMonitor));
+
+
+        public static void SetAttach(DependencyObject dp, bool value)
+        {
+            dp.SetValue(AttachProperty, value);
+        }
+
+        public static bool GetAttach(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(AttachProperty);
+        }
+
+        public static string GetPassword(DependencyObject dp)
+        {
+            return (string)dp.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject dp, string value)
+        {
+            dp.SetValue(PasswordProperty, value);
+        }
+
+        private static bool GetIsUpdating(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(IsUpdatingProperty);
+        }
+
+        private static void SetIsUpdating(DependencyObject dp, bool value)
+        {
+            dp.SetValue(IsUpdatingProperty, value);
+        }
+
+        private static void OnPasswordPropertyChanged(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            System.Windows.Controls.PasswordBox passwordBox = sender as System.Windows.Controls.PasswordBox;
+            passwordBox.PasswordChanged -= MyPasswordChanged;
+
+            if (!(bool)GetIsUpdating(passwordBox))
+            {
+                passwordBox.Password = (string)e.NewValue;
+            }
+            passwordBox.PasswordChanged += MyPasswordChanged;
+        }
+
+        private static void Attach(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            System.Windows.Controls.PasswordBox passwordBox = sender as System.Windows.Controls.PasswordBox;
+
+            if (passwordBox == null)
+                return;
+
+            if ((bool)e.OldValue)
+            {
+                passwordBox.PasswordChanged -= MyPasswordChanged;
+            }
+
+            if ((bool)e.NewValue)
+            {
+                passwordBox.PasswordChanged += MyPasswordChanged;
+            }
+        }
+
+        private static void MyPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.PasswordBox passwordBox = sender as System.Windows.Controls.PasswordBox;
+            SetIsUpdating(passwordBox, true);
+            SetPassword(passwordBox, passwordBox.Password);
+            SetIsUpdating(passwordBox, false);
+        }
     }
 }
