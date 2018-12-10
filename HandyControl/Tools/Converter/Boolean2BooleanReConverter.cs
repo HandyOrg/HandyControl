8200e746af7c287d603cc58fda2e7cdd1a7bbcb9
NaBian <836904362@qq.com>
2018-12-10 20:07:53 +0800

    fix bug

--- HandyControl/Tools/Converter/Boolean2BooleanReConverter.cs
+++ HandyControl/Tools/Converter/Boolean2BooleanReConverter.cs
@@ -17,7 +17,11 @@ namespace HandyControl.Tools.Converter
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            return value;
         }
     }
 }
