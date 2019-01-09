71f587c8159907f2d467667bc6757997b8115b68
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-01-09 15:37:21 +0330

    add drag property for windows

---
+++ HandyControl/Controls/Attach/DragWindows.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace HandyControl.Controls
+{
+    /// <summary>
+    /// Enable drag for a control
+    /// </summary>
+    public static class DragWindows
+    {
+        public static readonly DependencyProperty EnableDragProperty = DependencyProperty.RegisterAttached(
+            "EnableDrag",
+            typeof(bool),
+            typeof(DragWindows),
+            new PropertyMetadata(default(bool), OnLoaded));
+
+        private static void OnLoaded(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var uiElement = dependencyObject as UIElement;
+            if (uiElement == null || (dependencyPropertyChangedEventArgs.NewValue is bool) == false)
+            {
+                return;
+            }
+
+            if ((bool) dependencyPropertyChangedEventArgs.NewValue)
+            {
+                uiElement.MouseMove += UiElementOnMouseMove;
+            }
+            else
+            {
+                uiElement.MouseMove -= UiElementOnMouseMove;
+            }
+
+        }
+
+        private static void UiElementOnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (sender is UIElement uiElement && mouseEventArgs.LeftButton == MouseButtonState.Pressed)
+            {
+                DependencyObject parent = uiElement;
+                int avoidInfiniteLoop = 0;
+                // Search up the visual tree to find the first parent window.
+                while ((parent is Window) == false)
+                {
+                    parent = VisualTreeHelper.GetParent(parent);
+                    avoidInfiniteLoop++;
+                    if (avoidInfiniteLoop == 1000)
+                    {
+                        // Something is wrong - we could not find the parent window.
+                        return;
+                    }
+                }
+
+                var window = parent as Window;
+                window.DragMove();
+            }
+        }
+
+        public static void SetEnableDrag(DependencyObject element, bool value)
+        {
+            element.SetValue(EnableDragProperty, value);
+        }
+
+        public static bool GetEnableDrag(DependencyObject element)
+        {
+            return (bool) element.GetValue(EnableDragProperty);
+        }
+    }
+}
