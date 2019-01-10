004ae9422bef11a48157385c9ffc6f0e9185a20b
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-01-10 18:39:44 +0330

    add DragWindow

--- HandyControlDemo/Data/MessageToken.cs
+++ HandyControlDemo/Data/MessageToken.cs
@@ -64,6 +64,8 @@
 	
         public static readonly string BlurWindow = nameof(BlurWindow);
 	
+        public static readonly string NoClientDragableWindow = nameof(NoClientDragableWindow);
+	
         public static readonly string RateDemoCtl = nameof(RateDemoCtl);
 	
         public static readonly string ShieldDemoCtl = nameof(ShieldDemoCtl);
