83731484c9cff3bac683acc6b0f8622954f03d7b
NaBian <836904362@qq.com>
2019-01-11 14:02:07 +0800

    update version

--- HandyControl/Properties/AssemblyInfo.cs
+++ HandyControl/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.5.1.*")]
+[assembly: AssemblyVersion("1.5.2.*")]
