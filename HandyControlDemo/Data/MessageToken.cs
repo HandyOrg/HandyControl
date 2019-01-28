8923dce4430458d0a42b658477b75b061527a985
NaBian <836904362@qq.com>
2019-01-28 18:58:25 +0800

    init branch

--- HandyControlDemo/Data/MessageToken.cs
+++ HandyControlDemo/Data/MessageToken.cs
@@ -80,6 +80,8 @@
 	
         public static readonly string TransferDemoCtl = nameof(TransferDemoCtl);
 	
+        public static readonly string ChatBubbleDemoCtl = nameof(ChatBubbleDemoCtl);
+	
         public static readonly string ButtonDemoCtl = nameof(ButtonDemoCtl);
 
         public static readonly string ToggleButtonDemoCtl = nameof(ToggleButtonDemoCtl);
--- HandyControlDemo/Properties/Langs/Lang.Designer.cs
+++ HandyControlDemo/Properties/Langs/Lang.Designer.cs
@@ -47,7 +47,7 @@ namespace HandyControlDemo.Properties.Langs {
         }
         
         /// <summary>
-        ///   使用此强类型资源类，为所有资源查找
+        ///   重写当前线程的 CurrentUICulture 属性
         ///   重写当前线程的 CurrentUICulture 属性。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
@@ -174,6 +174,15 @@ namespace HandyControlDemo.Properties.Langs {
         public static string ChangeLangAsk {
             get {
                 return ResourceManager.GetString("ChangeLangAsk", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 对话气泡 的本地化字符串。
+        /// </summary>
+        public static string ChatBubble {
+            get {
+                return ResourceManager.GetString("ChatBubble", resourceCulture);
             }
         }
         
