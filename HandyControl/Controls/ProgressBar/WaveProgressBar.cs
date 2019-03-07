4c66d0dcf936df0ca31b1cd3158e2613507c985d
NaBian <836904362@qq.com>
2019-03-07 23:37:34 +0800

    add WaveProgressBar

--- HandyControl/Controls/ProgressBar/WaveProgressBar.cs
+++ HandyControl/Controls/ProgressBar/WaveProgressBar.cs
@@ -2,17 +2,73 @@
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using HandyControl.Data;
+using HandyControl.Expression.Drawing;
 
 namespace HandyControl.Controls
 {
+    /// <summary>
+    ///     波浪进度条
+    /// </summary>
+    [TemplatePart(Name = ElementWave, Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = ElementClip, Type = typeof(FrameworkElement))]
     public class WaveProgressBar : RangeBase
     {
+        private const string ElementWave = "PART_Wave";
+
+        private const string ElementClip = "PART_Clip";
+
+        private FrameworkElement _waveElement;
+
+        private const double TranslateTransformMinY = -20;
+
+        private double _translateTransformYRange;
+
+        private TranslateTransform _translateTransform;
+
+        public WaveProgressBar() => Loaded += (s, e) => UpdateWave(Value);
+
         static WaveProgressBar()
         {
-            FocusableProperty.OverrideMetadata(typeof(CircleProgressBar),
+            FocusableProperty.OverrideMetadata(typeof(WaveProgressBar),
                 new FrameworkPropertyMetadata(ValueBoxes.FalseBox));
-            MaximumProperty.OverrideMetadata(typeof(CircleProgressBar),
+            MaximumProperty.OverrideMetadata(typeof(WaveProgressBar),
                 new FrameworkPropertyMetadata(ValueBoxes.Double100Box));
+        }
+
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            base.OnValueChanged(oldValue, newValue);
+
+            UpdateWave(newValue);
+        }
+
+        private void UpdateWave(double value)
+        {
+            if (_translateTransform == null || MathHelper.IsVerySmall(Maximum)) return;
+            var scale = 1 - value / Maximum;
+            var y = _translateTransformYRange * scale + TranslateTransformMinY;
+            _translateTransform.Y = y;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _waveElement = GetTemplateChild(ElementWave) as FrameworkElement;
+            var clipElement = GetTemplateChild(ElementClip) as FrameworkElement;
+
+            if (_waveElement != null && clipElement != null)
+            {
+                _translateTransform = new TranslateTransform
+                {
+                    Y = clipElement.Height
+                };
+                _translateTransformYRange = clipElement.Height - TranslateTransformMinY;
+                _waveElement.RenderTransform = new TransformGroup
+                {
+                    Children = {_translateTransform}
+                };
+            }
         }
 
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
@@ -33,22 +89,31 @@ namespace HandyControl.Controls
             set => SetValue(ShowTextProperty, value);
         }
 
-        public static readonly DependencyProperty WaveBrushProperty = DependencyProperty.Register(
-            "WaveBrush", typeof(Brush), typeof(WaveProgressBar), new PropertyMetadata(default(Brush)));
+        public static readonly DependencyProperty WaveFillProperty = DependencyProperty.Register(
+            "WaveFill", typeof(Brush), typeof(WaveProgressBar), new PropertyMetadata(default(Brush)));
 
-        public Brush WaveBrush
+        public Brush WaveFill
         {
-            get => (Brush)GetValue(WaveBrushProperty);
-            set => SetValue(WaveBrushProperty, value);
+            get => (Brush) GetValue(WaveFillProperty);
+            set => SetValue(WaveFillProperty, value);
         }
 
-        public static readonly DependencyProperty WaveDiameterProperty = DependencyProperty.Register(
-            "WaveDiameter", typeof(double), typeof(WaveProgressBar), new PropertyMetadata(default(double)));
+        public static readonly DependencyProperty WaveThicknessProperty = DependencyProperty.Register(
+            "WaveThickness", typeof(double), typeof(WaveProgressBar), new PropertyMetadata(ValueBoxes.Double0Box));
 
-        public double WaveDiameter
+        public double WaveThickness
         {
-            get => (double)GetValue(WaveDiameterProperty);
-            set => SetValue(WaveDiameterProperty, value);
+            get => (double) GetValue(WaveThicknessProperty);
+            set => SetValue(WaveThicknessProperty, value);
+        }
+
+        public static readonly DependencyProperty WaveStrokeProperty = DependencyProperty.Register(
+            "WaveStroke", typeof(Brush), typeof(WaveProgressBar), new PropertyMetadata(default(Brush)));
+
+        public Brush WaveStroke
+        {
+            get => (Brush) GetValue(WaveStrokeProperty);
+            set => SetValue(WaveStrokeProperty, value);
         }
     }
 }
