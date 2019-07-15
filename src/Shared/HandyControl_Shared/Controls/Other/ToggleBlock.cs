4f9cc3a4720810e1fd48da43cccf80fe3bb23c37
NaBian <836904362@qq.com>
2019-07-15 23:10:08 +0800

    add ToggleBlock

---
+++ src/Shared/HandyControl_Shared/Controls/Other/ToggleBlock.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using HandyControl.Data;
+
+namespace HandyControl.Controls
+{
+    /// <summary>
+    ///     切换块
+    /// </summary>
+    /// <remarks>
+    ///     在不需要命中测试的情况下可代替ToggleButton或CheckBox，以减少内存用量。
+    /// </remarks>
+    public class ToggleBlock : Control
+    {
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register(
+            "IsChecked", typeof(bool?), typeof(ToggleBlock), new FrameworkPropertyMetadata(ValueBoxes.FalseBox,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault | FrameworkPropertyMetadataOptions.Journal));
+
+        [Category("Appearance")]
+        [TypeConverter(typeof(NullableBoolConverter))]
+        [Localizability(LocalizationCategory.None, Readability = Readability.Unreadable)]
+        public bool? IsChecked
+        {
+            get
+            {
+                // Because Nullable<bool> unboxing is very slow (uses reflection) first we cast to bool
+                var value = GetValue(IsCheckedProperty);
+                // ReSharper disable once RedundantExplicitNullableCreation
+                return value == null ? new bool?() : new bool?((bool)value);
+            }
+            set => SetValue(IsCheckedProperty, value.HasValue ? ValueBoxes.BooleanBox(value.Value) : null);
+        }
+
+        public static readonly DependencyProperty CheckedContentProperty = DependencyProperty.Register(
+            "CheckedContent", typeof(object), typeof(ToggleBlock), new PropertyMetadata(default(object)));
+
+        public object CheckedContent
+        {
+            get => GetValue(CheckedContentProperty);
+            set => SetValue(CheckedContentProperty, value);
+        }
+
+        public static readonly DependencyProperty UnCheckedContentProperty = DependencyProperty.Register(
+            "UnCheckedContent", typeof(object), typeof(ToggleBlock), new PropertyMetadata(default(object)));
+
+        public object UnCheckedContent
+        {
+            get => GetValue(UnCheckedContentProperty);
+            set => SetValue(UnCheckedContentProperty, value);
+        }
+
+        public static readonly DependencyProperty IndeterminateContentProperty = DependencyProperty.Register(
+            "IndeterminateContent", typeof(object), typeof(ToggleBlock), new PropertyMetadata(default(object)));
+
+        public object IndeterminateContent
+        {
+            get => GetValue(IndeterminateContentProperty);
+            set => SetValue(IndeterminateContentProperty, value);
+        }
+    }
+}
