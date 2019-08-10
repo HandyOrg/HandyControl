9c4fb33c557a38ceb6830b3c9fde2a248bfc0135
NaBian <836904362@qq.com>
2019-08-10 21:24:37 +0800

    add HoneycombPanel

--- src/Shared/HandyControlDemo_Shared/App.xaml.cs
+++ src/Shared/HandyControlDemo_Shared/App.xaml.cs
@@ -9,9 +9,6 @@ using HandyControlDemo.Tools;
 
 namespace HandyControlDemo
 {
-    /// <summary>
-    /// App.xaml 的交互逻辑
-    /// </summary>
     public partial class App
     {
         protected override void OnStartup(StartupEventArgs e)
