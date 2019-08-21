d69b875fd27c51bcdcef2e8e7c8423b0bedca6e0
NaBian <836904362@qq.com>
2019-08-21 22:24:31 +0800

    update version to v2.2.0

--- src/Shared/HandyControl_Shared/Properties/AssemblyInfo.cs
+++ src/Shared/HandyControl_Shared/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyVersion("2.2.0")]
 #endif
 
 [assembly: XmlnsDefinition("https://handyorg.github.io/handycontrol", "HandyControl.Controls")]
