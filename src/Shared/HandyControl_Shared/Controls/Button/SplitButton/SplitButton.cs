1b49c516c32cdfe2081621809fb682fa876204d5
NaBian <836904362@qq.com>
2019-09-05 00:14:50 +0800

    add SplitButtonItem

---
+++ src/Shared/HandyControl_Shared/Controls/Button/SplitButton/SplitButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using HandyControl.Data.Enum;
+
+namespace HandyControl.Controls
+{
+    public class SplitButton : ItemsControl
+    {
+        public static readonly DependencyProperty HitModeProperty = DependencyProperty.Register(
+            "HitMode", typeof(MouseHitMode), typeof(SplitButton), new PropertyMetadata(default(MouseHitMode)));
+
+        public MouseHitMode HitMode
+        {
+            get => (MouseHitMode) GetValue(HitModeProperty);
+            set => SetValue(HitModeProperty, value);
+        }
+
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(
+            "Content", typeof(object), typeof(SplitButton), new PropertyMetadata(default(object)));
+
+        public object Content
+        {
+            get => GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+
+        public static readonly DependencyProperty ContentTemplateProperty = DependencyProperty.Register(
+            "ContentTemplate", typeof(DataTemplate), typeof(SplitButton), new PropertyMetadata(default(DataTemplate)));
+
+        public DataTemplate ContentTemplate
+        {
+            get => (DataTemplate) GetValue(ContentTemplateProperty);
+            set => SetValue(ContentTemplateProperty, value);
+        }
+
+        public static readonly DependencyProperty MaxDropDownHeightProperty = DependencyProperty.Register(
+            "MaxDropDownHeight", typeof(double), typeof(SplitButton), new PropertyMetadata(SystemParameters.PrimaryScreenHeight / 3.0));
+
+        public double MaxDropDownHeight
+        {
+            get => (double) GetValue(MaxDropDownHeightProperty);
+            set => SetValue(MaxDropDownHeightProperty, value);
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item) => item is SplitButtonItem;
+
+        protected override DependencyObject GetContainerForItemOverride() => new SplitButtonItem();
+    }
+}
---
+++ src/Shared/HandyControl_Shared/Tools/Converter/ThicknessSplitConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HandyControl.Tools.Converter
+{
+    public class ThicknessSplitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Thickness cornerRadius)
+            {
+                if (parameter is string str)
+                {
+                    var arr = str.Split(',');
+                    if (arr.Length != 4) return cornerRadius;
+
+                    return new CornerRadius(arr[0].Equals("1") ? cornerRadius.Left : 0,
+                        arr[1].Equals("1") ? cornerRadius.Top : 0,
+                        arr[1].Equals("1") ? cornerRadius.Right : 0,
+                        arr[1].Equals("1") ? cornerRadius.Bottom : 0);
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
