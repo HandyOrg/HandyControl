4a335f46cb999c6e7a7fedde202bcd4fa2707659
NaBian <836904362@qq.com>
2019-10-31 00:48:47 +0800

    support for doc

--- doc/source/handycontrol/attach/iconSwitchElement/index.md
+++ doc/source/handycontrol/attach/iconSwitchElement/index.md
@@ -1,5 +1,13 @@
 ---
-title: 建设中
+title: IconSwitchElement 可切换图标的元素
 ---
 
-建设中
+{% note info no-icon %}
+继承自 [IconElement](https://handyorg.github.io/handycontrol/attach/iconElement/)
+{% endnote %}
+
+# 属性
+
+| 名称 | 用途 |
+|-|-|
+| GeometrySelected | 选中时的几何形状 |
--- src/Shared/HandyControlDemo_Shared/App.xaml.cs
+++ src/Shared/HandyControlDemo_Shared/App.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Net;
 using System.Security.Authentication;
+using System.Threading;
 using System.Windows;
 using HandyControl.Data;
 using HandyControl.Tools;
@@ -11,9 +15,35 @@ namespace HandyControlDemo
 {
     public partial class App
     {
+        // ReSharper disable once NotAccessedField.Local
+        [SuppressMessage("代码质量", "IDE0052:删除未读的私有成员", Justification = "<挂起>")]
+        private static Mutex AppMutex;
+
+        public App()
+        {
+            AppMutex = new Mutex(true, "HandyControlDemo", out var createdNew);
+            
+            if (!createdNew)
+            {
+                var current = Process.GetCurrentProcess();
+
+                foreach (var process in Process.GetProcessesByName(current.ProcessName))
+                {
+                    if (process.Id != current.Id)
+                    {
+                        Win32Helper.SetForegroundWindow(process.MainWindowHandle);
+                        break;
+                    }
+                }
+                Shutdown();
+            }
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            UpdateRegistry();
 
             ShutdownMode = ShutdownMode.OnMainWindowClose;
             GlobalData.Init();
@@ -53,6 +83,31 @@ namespace HandyControlDemo
                 Source = new Uri("pack://application:,,,/HandyControlDemo;component/Resources/Themes/Theme.xaml")
             });
             Current.MainWindow?.OnApplyTemplate();
+        }
+
+        private void UpdateRegistry()
+        {
+            var processModule = Process.GetCurrentProcess().MainModule;
+            if (processModule != null)
+            {
+                var registryFilePath = $"{Path.GetDirectoryName(processModule.FileName)}\\Registry.reg";
+                if (!File.Exists(registryFilePath))
+                {
+                    var streamResourceInfo = GetResourceStream(new Uri("pack://application:,,,/Resources/Registry.txt"));
+                    if (streamResourceInfo != null)
+                    {
+                        using var reader = new StreamReader(streamResourceInfo.Stream);
+                        var registryStr = reader.ReadToEnd();
+                        var newRegistryStr = registryStr.Replace("#", processModule.FileName.Replace("\\", "\\\\"));
+                        File.WriteAllText(registryFilePath, newRegistryStr);
+                        Process.Start(new ProcessStartInfo("cmd", $"/c {registryFilePath}")
+                        {
+                            UseShellExecute = false,
+                            CreateNoWindow = true
+                        });
+                    }
+                }
+            }
         }
     }
 }
