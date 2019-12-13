f1dca283f84231cd3c09e426fa0a1810da89c42a
NaBian <836904362@qq.com>
2019-12-14 01:33:32 +0800

    add ColorComplementEffect

---
+++ src/Shared/HandyControl_Shared/Media/Effects/ColorComplementEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace HandyControl.Media.Effects
+{
+    public class ColorComplementEffect : ShaderEffect
+    {
+        public static readonly DependencyProperty InputProperty = RegisterPixelShaderSamplerProperty("Input", typeof(ColorComplementEffect), 0);
+
+        private static readonly PixelShader Shader;
+
+        static ColorComplementEffect()
+        {
+            Shader = new PixelShader
+            {
+                UriSource = new Uri("pack://application:,,,/HandyControl;component/Resources/Effects/ColorComplementEffect.ps")
+            };
+        }
+
+        public ColorComplementEffect()
+        {
+            PixelShader = Shader;
+            UpdateShaderValue(InputProperty);
+        }
+
+        public Brush Input
+        {
+            get => (Brush)GetValue(InputProperty);
+            set => SetValue(InputProperty, value);
+        }
+    }
+}
