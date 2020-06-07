e67c04bf82b0ae8493c8724d0212e5aa513a7b1c
NaBian <836904362@qq.com>
2020-06-07 17:56:55 +0800

    update demo

--- src/Shared/HandyControlDemo_Shared/Data/Model/DemoItemModel.cs
+++ src/Shared/HandyControlDemo_Shared/Data/Model/DemoItemModel.cs
@@ -1,6 +1,8 @@
-﻿namespace HandyControlDemo.Data
+﻿using GalaSoft.MvvmLight;
+
+namespace HandyControlDemo.Data
 {
-    public class DemoItemModel
+    public class DemoItemModel : ObservableObject
     {
         public string Name { get; set; }
 
@@ -9,5 +11,17 @@
         public string ImageName { get; set; }
 
         public bool IsNew { get; set; }
+
+        private bool _isVisible = true;
+
+        public bool IsVisible
+        {
+            get => _isVisible;
+#if NET40
+            set => Set(nameof(IsVisible), ref _isVisible, value);
+#else
+            set => Set(ref _isVisible, value);
+#endif
+        }
     }
 }
