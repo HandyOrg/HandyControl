e2fd587d61b3a73993e5a74de6668ce033855ff9
Acoris <731984870@qq.com>
2020-06-15 22:13:44 +0800

    修复了一些bug

--- src/Shared/HandyControl_Shared/Controls/Tag/TagManager.cs
+++ src/Shared/HandyControl_Shared/Controls/Tag/TagManager.cs
@@ -37,6 +37,36 @@ namespace HandyControl.Controls
             }
         }
 
+        internal static void RemoveHandler(DependencyObject d, RoutedEvent routedEvent, Delegate handler)
+        {
+            if (d == null)
+            {
+                throw new ArgumentNullException("d");
+            }
+            UIElement uIElement = d as UIElement;
+            if (uIElement != null)
+            {
+                uIElement.RemoveHandler(routedEvent, handler);
+            }
+            else
+            {
+                ContentElement contentElement = d as ContentElement;
+                if (contentElement != null)
+                {
+                    contentElement.RemoveHandler(routedEvent, handler);
+                }
+                else
+                {
+                    UIElement3D uIElement3D = d as UIElement3D;
+                    if (uIElement3D == null)
+                    {
+                        throw new ArgumentException("Invalid_IInputElement");
+                    }
+                    uIElement3D.RemoveHandler(routedEvent, handler);
+                }
+            }
+        }
+
         public static void AddTagClosedEventHandler(DependencyObject element, RoutedEventHandler handler)
         {
             if (element == null)
@@ -54,7 +84,7 @@ namespace HandyControl.Controls
                 throw new ArgumentNullException("element");
             }
 
-            AddHandler(element, Tag.ClosedEvent, handler);
+            RemoveHandler(element, Tag.ClosedEvent, handler);
         }
 
 
@@ -64,14 +94,29 @@ namespace HandyControl.Controls
             return (bool)obj.GetValue(CanCloseTagProperty);
         }
 
+
         public static void SetCanCloseTag(DependencyObject obj, bool value)
         {
-            AddTagClosedEventHandler(obj, new RoutedEventHandler(DefaultTagClosed));
             obj.SetValue(CanCloseTagProperty, value);
         }
 
+        public static void OnCanCloseTagPropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is bool flag)
+            {
+                if (flag)
+                {
+                    AddTagClosedEventHandler(obj, DefaultTagClosed);
+                }
+                else
+                {
+                    RemoveTagClosedEventHandler(obj, DefaultTagClosed);
+                }
+            }
+        }
+
         public static readonly DependencyProperty CanCloseTagProperty =
-            DependencyProperty.RegisterAttached("ShadowCanCloseTag", typeof(bool), typeof(TagManager), new PropertyMetadata(false));
+            DependencyProperty.RegisterAttached("ShadowCanCloseTag", typeof(bool), typeof(TagManager), new PropertyMetadata(false,new PropertyChangedCallback(OnCanCloseTagPropertyChanged)));
 
 
         private static void DefaultTagClosed(object sender, RoutedEventArgs e)
