8e91d900a379fc29505a8cb3c0049318aee1cceb
Mahdi Hosseini <mahdidvb72@gmail.com>
2020-12-04 10:24:11 +0330

    Fix #551

--- src/Shared/HandyControl_Shared/Controls/TabControl/TabControl.cs
+++ src/Shared/HandyControl_Shared/Controls/TabControl/TabControl.cs
@@ -331,7 +331,8 @@ namespace HandyControl.Controls
                         Header = item.Header,
                         Width = TabItemWidth,
                         IsChecked = item.IsSelected,
-                        IsCheckable = true
+                        IsCheckable = true,
+                        IsEnabled = item.IsEnabled
                     };
 
                     menuItem.Click += delegate
