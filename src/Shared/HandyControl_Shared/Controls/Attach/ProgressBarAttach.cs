a9139d413a98e9234f4d303638411163409fd48f
Mahdi <mahdidvb72@gmail.com>
2021-03-09 18:27:04 +0330

    Add Issue #705

---
+++ src/Shared/HandyControl_Shared/Controls/Attach/ProgressBarAttach.cs
@@ -0,0 +1,17 @@
+﻿using HandyControl.Data;
+using System.Windows;
+
+namespace HandyControl.Controls
+{
+    public class ProgressBarAttach
+    {
+        public static readonly DependencyProperty ShowTextProperty = DependencyProperty.RegisterAttached(
+            "ShowText", typeof(bool), typeof(ProgressBarAttach), new PropertyMetadata(ValueBoxes.TrueBox));
+
+        public static void SetShowText(DependencyObject element, bool value)
+            => element.SetValue(ShowTextProperty, ValueBoxes.BooleanBox(value));
+
+        public static bool GetShowText(DependencyObject element)
+            => (bool) element.GetValue(ShowTextProperty);
+    }
+}
