a484306f6895aa06337cfe4b94aa79d41683d26a
NaBian <836904362@qq.com>
2022-06-17 22:59:31 +0800

    chore(style): add ListView small style.

---
+++ src/Shared/HandyControl_Shared/Controls/Attach/GridViewAttach.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using HandyControl.Data;
+
+namespace HandyControl.Controls;
+
+public class GridViewAttach
+{
+    public static readonly DependencyProperty ColumnHeaderHeightProperty = DependencyProperty.RegisterAttached(
+        "ColumnHeaderHeight", typeof(double), typeof(GridViewAttach), new PropertyMetadata(ValueBoxes.Double0Box));
+
+    public static void SetColumnHeaderHeight(DependencyObject element, double value)
+        => element.SetValue(ColumnHeaderHeightProperty, value);
+
+    public static double GetColumnHeaderHeight(DependencyObject element)
+        => (double) element.GetValue(ColumnHeaderHeightProperty);
+}
