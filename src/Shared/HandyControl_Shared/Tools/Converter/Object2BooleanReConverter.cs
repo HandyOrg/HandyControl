f4d47f5af3aee4baee2d679d77c9a699eb2380ce
NaBian <836904362@qq.com>
2022-09-03 20:22:19 +0800

    chore: add Object2BooleanReConverter.

--- src/Shared/HandyControl_Shared/Tools/Converter/Object2BooleanReConverter.cs
+++ src/Shared/HandyControl_Shared/Tools/Converter/Object2BooleanReConverter.cs
@@ -4,9 +4,9 @@ using System.Windows.Data;
 
 namespace HandyControl.Tools.Converter;
 
-public class Object2BooleanConverter : IValueConverter
+public class Object2BooleanReConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is not null;
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is null;
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
