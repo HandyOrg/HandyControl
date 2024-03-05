b0d7cc86361aad07cf3bd4d2ea96df3a005ea960
NaBian <836904362@qq.com>
2024-03-05 10:40:47 +0800

    enhance: add IsMouseWheelEnabled attach property.

---
+++ src/Shared/HandyControl_Shared/Controls/Attach/ComboBoxAttach.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using HandyControl.Data;
+
+namespace HandyControl.Controls;
+
+public class ComboBoxAttach
+{
+    public static readonly DependencyProperty IsMouseWheelEnabledProperty = DependencyProperty.RegisterAttached(
+        "IsMouseWheelEnabled", typeof(bool), typeof(ComboBoxAttach), new PropertyMetadata(ValueBoxes.TrueBox, OnIsMouseWheelEnabledChanged));
+
+    private static void OnIsMouseWheelEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not System.Windows.Controls.ComboBox comboBox)
+        {
+            return;
+        }
+
+        if (!(bool) e.NewValue)
+        {
+            comboBox.PreviewMouseWheel += OnComboBoxPreviewMouseWheel;
+        }
+        else
+        {
+            comboBox.PreviewMouseWheel -= OnComboBoxPreviewMouseWheel;
+        }
+
+        return;
+
+        static void OnComboBoxPreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs args)
+        {
+            if (sender is System.Windows.Controls.ComboBox { IsDropDownOpen: true })
+            {
+                return;
+            }
+
+            args.Handled = true;
+        }
+    }
+
+    public static void SetIsMouseWheelEnabled(DependencyObject element, bool value)
+        => element.SetValue(IsMouseWheelEnabledProperty, ValueBoxes.BooleanBox(value));
+
+    public static bool GetIsMouseWheelEnabled(DependencyObject element)
+        => (bool) element.GetValue(IsMouseWheelEnabledProperty);
+}
