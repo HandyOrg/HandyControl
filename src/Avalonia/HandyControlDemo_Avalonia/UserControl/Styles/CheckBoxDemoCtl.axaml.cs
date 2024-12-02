9122671a4b57ca0f52e4a3e7a4d81408f0364a8a
NaBian <836904362@qq.com>
2024-12-02 23:21:51 +0800

    chore: add avalonia CheckBox demo.

---
+++ src/Avalonia/HandyControlDemo_Avalonia/UserControl/Styles/CheckBoxDemoCtl.axaml.cs
@@ -0,0 +1,9 @@
+namespace HandyControlDemo.UserControl;
+
+public partial class CheckBoxDemoCtl : Avalonia.Controls.UserControl
+{
+    public CheckBoxDemoCtl()
+    {
+        InitializeComponent();
+    }
+}
