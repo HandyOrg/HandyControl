c78e8545b858c292538334310eb4d06e87f586f7
NaBian <836904362@qq.com>
2024-12-15 15:03:34 +0800

    chore: add avalonia Label demo.

---
+++ src/Avalonia/HandyControlDemo_Avalonia/Tools/Converter/StringRepeatConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using Avalonia.Data.Converters;
+
+namespace HandyControlDemo.Tools.Converter;
+
+public class StringRepeatConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string strValue)
+        {
+            return string.Empty;
+        }
+
+        var builder = new StringBuilder();
+        int num;
+
+        switch (parameter)
+        {
+            case string numStr:
+                {
+                    if (!int.TryParse(numStr, out num))
+                    {
+                        return strValue;
+                    }
+
+                    break;
+                }
+            case int intValue:
+                num = intValue;
+                break;
+            default:
+                return strValue;
+        }
+
+        for (int i = 0; i < num; i++)
+        {
+            builder.Append(strValue);
+        }
+
+        return builder.ToString();
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ src/Avalonia/HandyControlDemo_Avalonia/UserControl/Styles/LabelDemo.axaml.cs
@@ -0,0 +1,9 @@
+namespace HandyControlDemo.UserControl;
+
+public partial class LabelDemo : Avalonia.Controls.UserControl
+{
+    public LabelDemo()
+    {
+        InitializeComponent();
+    }
+}
