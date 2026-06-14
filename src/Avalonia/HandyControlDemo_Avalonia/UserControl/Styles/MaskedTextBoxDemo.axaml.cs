21f90d60bba99e11ded3941d39d5d0389e252fa2
NaBian <836904362@qq.com>
2026-06-14 13:50:38 +0800

    feat: Add MaskedTextBox demo.

---
+++ src/Avalonia/HandyControlDemo_Avalonia/UserControl/Styles/MaskedTextBoxDemo.axaml.cs
@@ -0,0 +1,9 @@
+namespace HandyControlDemo.UserControl;
+
+public partial class MaskedTextBoxDemo : Avalonia.Controls.UserControl
+{
+    public MaskedTextBoxDemo()
+    {
+        InitializeComponent();
+    }
+}
