4d5f45389595d8381fd3cca6c010958d43dda0d8
NaBian <836904362@qq.com>
2026-06-14 15:50:00 +0800

    feat: Add DropDownButton styles and demos.

---
+++ src/Avalonia/HandyControlDemo_Avalonia/UserControl/Styles/DropDownButtonDemo.axaml.cs
@@ -0,0 +1,9 @@
+namespace HandyControlDemo.UserControl;
+
+public partial class DropDownButtonDemo : Avalonia.Controls.UserControl
+{
+    public DropDownButtonDemo()
+    {
+        InitializeComponent();
+    }
+}
