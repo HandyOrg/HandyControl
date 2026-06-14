3577b46e52bbc188f764a68adf1fcb9e34a41a7b
NaBian <836904362@qq.com>
2026-06-14 14:30:43 +0800

    chore: Add DatePicker theme.

---
+++ src/Avalonia/HandyControlDemo_Avalonia/UserControl/Styles/CalendarDatePickerDemo.axaml.cs
@@ -0,0 +1,9 @@
+namespace HandyControlDemo.UserControl;
+
+public partial class CalendarDatePickerDemo : Avalonia.Controls.UserControl
+{
+    public CalendarDatePickerDemo()
+    {
+        InitializeComponent();
+    }
+}
